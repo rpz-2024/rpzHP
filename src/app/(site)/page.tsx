d013f560d43b67f0b9683e36aa6506999083feac
Hirota723 <koki.programming@gmail.com>
2025-08-23 02:18:54 +0900

    design: 🎨 UIを一部修正

--- src/app/(site)/page.tsx
+++ src/app/(site)/page.tsx
@@ -6,6 +6,7 @@ import { StoresList } from "@/components/sections/StoresList";
 export default function Page() {
 	return (
 		<main className="lg:contents">
+			<Hero />
 			<NewsAccordion />
 			<StoresList />
 			<FloatingTopButton />
