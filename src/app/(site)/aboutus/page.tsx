a8648349e2eaf9e4a56cef66e9aa015ffa1f7aa3
rpz-2024 <t.saito@rpz.co.jp>
2025-09-08 14:26:22 +0900

    RPZの想いの項目修正

--- src/app/(site)/aboutus/page.tsx
+++ src/app/(site)/aboutus/page.tsx
@@ -1,11 +1,11 @@
 import type { Metadata } from "next";
-import { AboutusSection } from "@/components/sections/Aboutus";
+import { AboutusSection } from "@/components/sections/aboutus";
 
 export const metadata: Metadata = {
 	title: "RPZの想い｜株式会社RPZ",
 	description:
 		"株式会社RPZは「人を想い人々を支え志の高い目標を立ち向かえる組織」として、飲食を通じて皆様に感動体験を提供し続けています。",
-	alternates: { canonical: "/Aboutus" },
+	alternates: { canonical: "/aboutus" },
 };
 
 export default function Page() {
