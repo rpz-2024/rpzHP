600c5a185495b5a5a8198410b0c305804ac8ae60
rpz-2024 <t.saito@rpz.co.jp>
2025-10-08 11:44:50 +0900

    fix middleware.ts erra  page

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,9 +1,8 @@
 // src/middleware.ts
 import { NextResponse } from 'next/server';
-import type { NextRequest } from 'next/server';
 
 export const config = { matcher: ['/aboutus', '/aboutus/'] };
 
-export default function middleware(_req: NextRequest) {
-  return NextResponse.next(); // 何もしない（ページ処理に通す）
+export default function middleware() {
+  return NextResponse.next(); // そのままページへ通す
 }
